526aa1c001f72bc77b8dbb585b04debb4f6a30ba
abadug5793 <bavani7777@gmail.com>
2018-06-20 18:18:35 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,16 @@
-# Code your prompts here!
 
-# Try starting out with puts'ing a string.
+def letter
+  #Asking who are you inviting
+  puts "Who are you inviting?"
+  who = gets.chomp
+  #Asking for the party name
+  puts "What is the party name?"
+  party_name = gets.chomp
+  #date of party
+  puts "What is the date of the party?"
+  date = gets.chomp
+  puts "Is the party in the afternoon or evening?"
+  time = gets.chomp
+  if time == "afternoon"
+    puts "12PM"
+    
